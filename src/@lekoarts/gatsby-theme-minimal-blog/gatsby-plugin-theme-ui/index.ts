c505b6db4f9453c82858fc88433aad0a3d3cd42e
shpilson <kovalenko.jora@gmail.com>
2023-08-13 02:46:20 -0300

    change color toggle icon

--- src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.ts
+++ src/@lekoarts/gatsby-theme-minimal-blog/gatsby-plugin-theme-ui/index.ts
@@ -14,7 +14,7 @@ const theme = merge(tailwind, {
         initialColorModeName: `light`,
     },
     colors: {
-        primary: tailwind.colors.purple[7],
+        primary: tailwind.colors.yellow[5],
         secondary: `#5f6c80`,
         toggleIcon: tailwind.colors.gray[8],
         heading: tailwind.colors.black,
@@ -25,9 +25,9 @@ const theme = merge(tailwind, {
         modes: {
             dark: {
                 text: tailwind.colors.gray[4],
-                primary: tailwind.colors.purple[4],
+                primary: tailwind.colors.yellow[4],
                 secondary: `#8a9ab0`,
-                toggleIcon: tailwind.colors.gray[4],
+                toggleIcon: tailwind.colors.yellow[5],
                 background: `#1A202C`,
                 heading: tailwind.colors.white,
                 divide: tailwind.colors.gray[8],
